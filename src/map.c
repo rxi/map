d6c355fcfb35e5bf71905c0c28ccee34694b4a95
rxi <rxi@users.noreply.github.com>
2014-09-19 19:52:19 +0100

    Added copyright/license header to source files

--- src/map.c
+++ src/map.c
@@ -1,3 +1,10 @@
+/** 
+ * Copyright (c) 2014 rxi
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the MIT license. See LICENSE for details.
+ */
+
 #include <stdlib.h>
 #include <string.h>
 #include "map.h"
