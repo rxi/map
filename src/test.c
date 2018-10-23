03d6724749836d8f6a5569c00120bf26803fe809
Giovanni Cerretani <g.cerretani@caensys.com>
2018-10-23 10:25:57 +0200

    added test

---
+++ src/test.c
@@ -0,0 +1,25 @@
+#include "map.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef map_t(int) map_int_t;
+
+int main() {
+	map_int_t *map = malloc(sizeof(*map));
+	if (map == NULL)
+		return -1;
+	
+	const int value = 12;
+	const char key[] = "mykey";
+	
+	map_init(map, 16);
+	map_set(map, key, value);
+
+	int *value_get = map_get(map, key); 
+	
+	printf("%i\n", *value_get);
+	
+	free(map);
+	
+	return 0;
+}
