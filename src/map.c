c7e36a3251bc1c7efc93e0ab2fe9c171b2520548
Giovanni Cerretani <g.cerretani@caensys.com>
2018-10-23 10:16:22 +0200

    map_newnode() adjusted to be more readable (at cost of 2 additional call to malloc). indentation adjusted.

--- src/map.c
+++ src/map.c
@@ -8,214 +8,230 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdint.h>
+
 #include "map.h"
 
 struct map_node_t {
-  unsigned int hash;
-  void *value;
-  map_node_t *next;
-  /* char key[]; */
-  /* char value[]; */
+	unsigned int hash;
+	void *value;
+	char *key;
+	map_node_t *next;
 };
 
 
 static unsigned int map_hash(const char *str) {
-  unsigned int hash = 5381;
-  while (*str) {
-    hash = ((hash << 5) + hash) ^ *str++;
-  }
-  return hash;
+	unsigned int hash = 5381;
+	while (*str) {
+		hash = ((hash << 5) + hash) ^ *str++;
+	}
+	return hash;
 }
 
 
 static map_node_t *map_newnode(const char *key, void *value, int vsize) {
-  int ksize = strlen(key) + 1;
-  if (ksize > PTRDIFF_MAX) return NULL;
-  int voffset = ksize + ((sizeof(void*) - ksize) % sizeof(void*));
-  map_node_t *node = malloc(sizeof(*node) + voffset + vsize);
-  if (!node) return NULL;
-  memcpy(node + 1, key, ksize);
-  node->hash = map_hash(key);
-  node->value = ((char*) (node + 1)) + voffset;
-  memcpy(node->value, value, vsize);
-  return node;
+	map_node_t *node = malloc(sizeof(*node));
+	if (!node)
+		return NULL;
+	node->hash = map_hash(key);
+	node->key = strdup(key);
+	if (node->key == NULL) {
+		free(node);
+		return NULL;
+	}
+	node->value = malloc(vsize);
+	if (node->value == NULL) {
+		free(node->key);
+		free(node);
+		return NULL;
+	}
+	memcpy(node->value, value, vsize);
+	return node;
+}
+
+
+static void map_deletenode(map_node_t *node) {
+	if (node == NULL)
+		return;
+	free(node->value);
+	free(node->key);
+	free(node);
 }
 
 
 static int map_bucketidx(map_base_t *m, unsigned int hash) {
-  /* If the implementation is changed to allow a non-power-of-2 bucket count,
-   * the line below should be changed to use mod instead of AND */
-  return hash & (m->nbuckets - 1);
+	/* If the implementation is changed to allow a non-power-of-2 bucket count,
+	 * the line below should be changed to use mod instead of AND */
+	return hash & (m->nbuckets - 1);
 }
 
 
 static void map_addnode(map_base_t *m, map_node_t *node) {
-  int n = map_bucketidx(m, node->hash);
-  node->next = m->buckets[n];
-  m->buckets[n] = node;
+	int n = map_bucketidx(m, node->hash);
+	node->next = m->buckets[n];
+	m->buckets[n] = node;
 }
 
 
 static int map_resize(map_base_t *m, unsigned int nbuckets) {
-  map_node_t *nodes, *node, *next;
-  map_node_t **buckets;
-  /* Chain all nodes together */
-  nodes = NULL;
-  unsigned int i = m->nbuckets;
-  while (i--) {
-    node = (m->buckets)[i];
-    while (node) {
-      next = node->next;
-      node->next = nodes;
-      nodes = node;
-      node = next;
-    }
-  }
-  /* Reset buckets */
-  buckets = realloc(m->buckets, sizeof(*m->buckets) * nbuckets);
-  if (buckets != NULL) {
-    m->buckets = buckets;
-    m->nbuckets = nbuckets;
-  }
-  if (m->buckets) {
-    memset(m->buckets, 0, sizeof(*m->buckets) * m->nbuckets);
-    /* Re-add nodes to buckets */
-    node = nodes;
-    while (node) {
-      next = node->next;
-      map_addnode(m, node);
-      node = next;
-    }
-  }
-  /* Return error code if realloc() failed */
-  return (buckets == NULL) ? -1 : 0;
+	map_node_t *nodes, *node, *next;
+	map_node_t **buckets;
+	/* Chain all nodes together */
+	nodes = NULL;
+	unsigned int i = m->nbuckets;
+	while (i--) {
+		node = (m->buckets)[i];
+		while (node) {
+			next = node->next;
+			node->next = nodes;
+			nodes = node;
+			node = next;
+		}
+	}
+	/* Reset buckets */
+	buckets = realloc(m->buckets, sizeof(*m->buckets) * nbuckets);
+	if (buckets != NULL) {
+		m->buckets = buckets;
+		m->nbuckets = nbuckets;
+	}
+	if (m->buckets) {
+		memset(m->buckets, 0, sizeof(*m->buckets) * m->nbuckets);
+		/* Re-add nodes to buckets */
+		node = nodes;
+		while (node) {
+			next = node->next;
+			map_addnode(m, node);
+			node = next;
+		}
+	}
+	/* Return error code if realloc() failed */
+	return (buckets == NULL) ? -1 : 0;
 }
 
 
 static map_node_t **map_getref(map_base_t *m, const char *key) {
-  unsigned int hash = map_hash(key);
-  if (m->nbuckets > 0) {
-    map_node_t **next = &m->buckets[map_bucketidx(m, hash)];
-    while (*next) {
-      if ((*next)->hash == hash && !strcmp((char*) (*next + 1), key)) {
-        return next;
-      }
-      next = &(*next)->next;
-    }
-  }
-  return NULL;
+	unsigned int hash = map_hash(key);
+	if (m->nbuckets > 0) {
+		map_node_t **next = &m->buckets[map_bucketidx(m, hash)];
+		while (*next) {
+			if ((*next)->hash == hash && !strcmp((*next)->key, key)) {
+				return next;
+			}
+			next = &(*next)->next;
+		}
+	}
+	return NULL;
 }
 
 
 int map_init_(map_base_t *m, unsigned int initial_nbuckets) {
-  // Clear the memory.
-  memset(m, 0, sizeof(*m));
-  m->initialized = true;
-  // Pre-initialize buckets array only if initial_nbuckets is a power of 2.
-  // Anyway, it is reallocated automatically when needed.
-  if ((initial_nbuckets > 0) && !(initial_nbuckets & (initial_nbuckets - 1)))
-    return map_resize(m, initial_nbuckets);
-  else
-    return 0;
+	// Clear the memory.
+	memset(m, 0, sizeof(*m));
+	m->initialized = true;
+	// Pre-initialize buckets array only if initial_nbuckets is a power of 2.
+	// Anyway, it is reallocated automatically when needed.
+	if ((initial_nbuckets > 0) && !(initial_nbuckets & (initial_nbuckets - 1)))
+		return map_resize(m, initial_nbuckets);
+	else
+		return 0;
 }
 
 
 void map_deinit_(map_base_t *m) {
-  if (m == NULL)
-    return;
-  if (!m->initialized)
-    return;
-  unsigned int i = m->nbuckets;
-  while (i--) {
-    map_node_t *node = m->buckets[i];
-    while (node) {
-      map_node_t *next = node->next;
-      free(node);
-      node = next;
-    }
-  }
-  free(m->buckets);
-  m->initialized = false;
+	if (m == NULL)
+		return;
+	if (!m->initialized)
+		return;
+	unsigned int i = m->nbuckets;
+	while (i--) {
+		map_node_t *node = m->buckets[i];
+		while (node) {
+			map_node_t *next = node->next;
+			map_deletenode(node);
+			node = next;
+		}
+	}
+	free(m->buckets);
+	m->initialized = false;
 }
 
 
 void *map_get_(map_base_t *m, const char *key) {
-  if (m == NULL || key == NULL)
-    return NULL;
-  if (!m->initialized)
-    return NULL;
-  map_node_t **next = map_getref(m, key);
-  return next ? (*next)->value : NULL;
+	if (m == NULL || key == NULL)
+		return NULL;
+	if (!m->initialized)
+		return NULL;
+	map_node_t **next = map_getref(m, key);
+	return next ? (*next)->value : NULL;
 }
 
 
 int map_set_(map_base_t *m, const char *key, void *value, int vsize) {
-  if (m == NULL || key == NULL || value == NULL)
-    return -1;
-  if (!m->initialized)
-    return -1;
-  /* Find & replace existing node */
-  map_node_t **next = map_getref(m, key);
-  if (next) {
-    memcpy((*next)->value, value, vsize);
-    return 0;
-  }
-  /* Add new node */
-  map_node_t *node = map_newnode(key, value, vsize);
-  if (node == NULL) goto fail;
-  if (m->nnodes >= m->nbuckets) {
-    unsigned int n = (m->nbuckets > 0) ? (m->nbuckets << 1) : 1;
-    int err = map_resize(m, n);
-    if (err) goto fail;
-  }
-  map_addnode(m, node);
-  m->nnodes++;
-  return 0;
-  fail:
-  if (node) free(node);
-  return -1;
+	if (m == NULL || key == NULL || value == NULL)
+		return -1;
+	if (!m->initialized)
+		return -1;
+	/* Find & replace existing node */
+	map_node_t **next = map_getref(m, key);
+	if (next) {
+		memcpy((*next)->value, value, vsize);
+		return 0;
+	}
+	/* Add new node */
+	map_node_t *node = map_newnode(key, value, vsize);
+	if (node == NULL) goto fail;
+	if (m->nnodes >= m->nbuckets) {
+		unsigned int n = (m->nbuckets > 0) ? (m->nbuckets << 1) : 1;
+		int err = map_resize(m, n);
+		if (err) goto fail;
+	}
+	map_addnode(m, node);
+	m->nnodes++;
+	return 0;
+	fail:
+	map_deletenode(node);
+	return -1;
 }
 
 
 void map_remove_(map_base_t *m, const char *key) {
-  if (m == NULL || key == NULL)
-    return;
-  if (!m->initialized)
-    return;
-  map_node_t **next = map_getref(m, key);
-  if (next) {
-    map_node_t *node = *next;
-    *next = (*next)->next;
-    free(node);
-    m->nnodes--;
-  }
+	if (m == NULL || key == NULL)
+		return;
+	if (!m->initialized)
+		return;
+	map_node_t **next = map_getref(m, key);
+	if (next) {
+		map_node_t *node = *next;
+		*next = (*next)->next;
+		map_deletenode(node);
+		m->nnodes--;
+	}
 }
 
 
 map_iter_t map_iter_(void) {
-  map_iter_t iter;
-  iter.bucketidx = -1;
-  iter.node = NULL;
-  return iter;
+	map_iter_t iter;
+	iter.bucketidx = -1;
+	iter.node = NULL;
+	return iter;
 }
 
 
 const char *map_next_(map_base_t *m, map_iter_t *iter) {
-  if (m == NULL || iter == NULL)
-    return NULL;
-  if (!m->initialized)
-    return NULL;
-  if (iter->node) {
-    iter->node = iter->node->next;
-    if (iter->node == NULL) goto nextBucket;
-  } else {
-    nextBucket:
-    do {
-      if (++iter->bucketidx >= m->nbuckets) {
-        return NULL;
-      }
-      iter->node = m->buckets[iter->bucketidx];
-    } while (iter->node == NULL);
-  }
-  return (char*) (iter->node + 1);
+	if (m == NULL || iter == NULL)
+		return NULL;
+	if (!m->initialized)
+		return NULL;
+	if (iter->node) {
+		iter->node = iter->node->next;
+		if (iter->node == NULL) goto nextBucket;
+	} else {
+		nextBucket:
+		do {
+			if (++iter->bucketidx >= m->nbuckets) {
+				return NULL;
+			}
+			iter->node = m->buckets[iter->bucketidx];
+		} while (iter->node == NULL);
+	}
+	return iter->node->key;
 }
--- src/map.h
+++ src/map.h
@@ -17,46 +17,46 @@ struct map_node_t;
 typedef struct map_node_t map_node_t;
 
 typedef struct {
-  map_node_t **buckets;
-  unsigned int nbuckets, nnodes;
-  bool initialized;
+	map_node_t **buckets;
+	unsigned int nbuckets, nnodes;
+	bool initialized;
 } map_base_t;
 
 typedef struct {
-  unsigned int bucketidx;
-  map_node_t *node;
+	unsigned int bucketidx;
+	map_node_t *node;
 } map_iter_t;
 
 
 #define map_t(T)\
-  struct { map_base_t base; T *ref; T tmp; }
+	struct { map_base_t base; T *ref; T tmp; }
 
 
 #define map_init(m, initial_nbuckets)\
-  ( ((m) != NULL) ? map_init_(&(m)->base, initial_nbuckets) : -1 )
+	( ((m) != NULL) ? map_init_(&(m)->base, initial_nbuckets) : -1 )
 
 
 #define map_deinit(m)\
-  do { if ((m) != NULL) { map_deinit_(&(m)->base); } } while (0)
+	do { if ((m) != NULL) { map_deinit_(&(m)->base); } } while (0)
 
 
 #define map_get(m, key)\
-  ( ((m) != NULL) ? map_get_(&(m)->base, key) : NULL )
+	( ((m) != NULL) ? map_get_(&(m)->base, key) : NULL )
 
 
 #define map_set(m, key, value)\
-  ( ((m) != NULL) ? ((m)->tmp = (value), map_set_(&(m)->base, key, &(m)->tmp, sizeof((m)->tmp))) : -1 )
+	( ((m) != NULL) ? ((m)->tmp = (value), map_set_(&(m)->base, key, &(m)->tmp, sizeof((m)->tmp))) : -1 )
 
 #define map_remove(m, key)\
-  do { if ((m) != NULL) { map_remove_(&(m)->base, key); } } while (0)
+	do { if ((m) != NULL) { map_remove_(&(m)->base, key); } } while (0)
 
 
 #define map_iter(m)\
-  map_iter_()
+	map_iter_()
 
 
 #define map_next(m, iter)\
-  ( ((m) != NULL) ? map_next_(&(m)->base, iter) : NULL )
+	( ((m) != NULL) ? map_next_(&(m)->base, iter) : NULL )
 
 
 int map_init_(map_base_t *m, unsigned int initial_nbuckets);
