89d757c1b801c3573b075147aa45893778275da4
Giovanni Cerretani <g.cerretani@caensys.com>
2019-12-05 09:49:26 +0100

    test improved

--- src/test.c
+++ src/test.c
@@ -5,22 +5,31 @@
 typedef map_t(int) map_int_t;
 
 int main() {
+	int ret = 0;
+
 	map_int_t *map = malloc(sizeof(*map));
-	if (map == NULL)
-		return -1;
-	
+	if (map == NULL) {
+		ret = 1;
+		goto quit;
+	}
+
 	const int value = 12;
 	const char key[] = "mykey";
-	
+
 	map_init_reserve(map, 16);
 	map_set(map, key, value);
-	int *value_get = map_get(map, key); 
-	
-	if (value_get != NULL)
-		printf("%i\n", *value_get);
-	else
-		printf("Error\n");	
-	
+	int *value_get = map_get(map, key);
+
+	if (value_get == NULL) {
+		ret = 2;
+		goto quit;
+	}
+
+	if (*value_get != value) {
+		ret = 3;
+	}
+
+quit:
 	free(map);
-	return 0;
+	return ret;
 }
