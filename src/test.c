ef9576f4f7e2ac92568790dc941b3f2daf6de93a
Giovanni Cerretani <giovanni.cerretani@gmail.com>
2022-10-05 10:22:35 +0200

    Update test.c

--- src/test.c
+++ src/test.c
@@ -1,3 +1,10 @@
+/** 
+ * Copyright (c) 2019-2022 Giovanni Cerretani
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the MIT license. See LICENSE for details.
+ */
+
 #include "map.h"
 #include <stdio.h>
 #include <stdlib.h>
